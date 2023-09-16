dc8c0915fa5d504f11ebc882795967b5051d1226
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-09-16 18:40:01 +0200

    upload

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,17 +1,17 @@
-export const ALL_ARANGEMENTS = "http://localhost:4000/arangements"
-export const ARANGEMENTS_PUBLISHED_FREE = "http://localhost:4000/arangements?isPublished_like=true&state_like=free"
-export const DESTINATIONS_ENDPOINT = "http://localhost:4000/dropdown-destinations"
-export const GALERY_ENDPOINT = "http://localhost:4000/galery-images"
-export const TESTEMONIALS_ENDPOINT = "http://localhost:4000/testemonials?_limit=3"
+export const ALL_ARANGEMENTS = "https://infinity-travel-database.glitch.me/arangements"
+export const ARANGEMENTS_PUBLISHED_FREE = "https://infinity-travel-database.glitch.me/arangements?isPublished_like=true&state_like=free"
+export const DESTINATIONS_ENDPOINT = "https://infinity-travel-database.glitch.me/dropdown-destinations"
+export const GALERY_ENDPOINT = "https://infinity-travel-database.glitch.me/galery-images"
+export const TESTEMONIALS_ENDPOINT = "https://infinity-travel-database.glitch.me/testemonials?_limit=3"
 
-export const GRUPNI_INFO = "http://localhost:4000/grupniPatuvanjaInfo"
-export const OPSTI_USLOVI_INFO = "http://localhost:4000/opsti-uslovi-info"
-export const PATNICKO_INFO = "http://localhost:4000/patnicko-osiguruvanje-info"
-export const ABOUT_US_MID_SECTION_DATA = "http://localhost:4000/about-us-mid-section-data"    
+export const GRUPNI_INFO = "https://infinity-travel-database.glitch.me/grupniPatuvanjaInfo"
+export const OPSTI_USLOVI_INFO = "https://infinity-travel-database.glitch.me/opsti-uslovi-info"
+export const PATNICKO_INFO = "https://infinity-travel-database.glitch.me/patnicko-osiguruvanje-info"
+export const ABOUT_US_MID_SECTION_DATA = "https://infinity-travel-database.glitch.me/about-us-mid-section-data"    
 
 // POST REQUESTS
-export const NEWSLETTER_ENDPOINT = "http://localhost:4000/newsletter"
-export const CONTACT_ENPOINT = "http://localhost:4000/contact-form-requests"
-export const FLY_TICKET_REQUEST_ENDPOINT = 'http://localhost:4000/ticket-search-contact-info'
+export const NEWSLETTER_ENDPOINT = "https://infinity-travel-database.glitch.me/newsletter"
+export const CONTACT_ENPOINT = "https://infinity-travel-database.glitch.me/contact-form-requests"
+export const FLY_TICKET_REQUEST_ENDPOINT = 'https://infinity-travel-database.glitch.me/ticket-search-contact-info'
 
 
